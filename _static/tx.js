f93dae9bf13adca7063ff3a372d7e057fb6afb3f
GitHub Action <action@github.com>
2022-02-20 18:01:51 +0000

    Add changes for 6d7c248ac643d9c3eb6a1fe61f082cc354af5d37

--- _static/tx.js
+++ _static/tx.js
@@ -1,11 +1,13 @@
-const s = document.createElement('script');
-s.setAttribute("src","https://cdn.jsdelivr.net/npm/@transifex/native/dist/browser.native.min.js");
-s.setAttribute("type","text/javascript");
-s.onload=function(){
-const tx = Transifex.tx;
-const t = Transifex.t;
-tx.init({
-  token: '1/5c5efef5446b45f57b080697f32e5a6903d0503e',
-});
-}
+window.onload= function(){
+  const s = document.createElement('script');
+  s.setAttribute("src","https://cdn.jsdelivr.net/npm/@transifex/native/dist/browser.native.min.js");
+  s.setAttribute("type","text/javascript");
+  s.onload=function(){
+  const tx = Transifex.tx;
+  const t = Transifex.t;
+  tx.init({
+    token: '1/5c5efef5446b45f57b080697f32e5a6903d0503e',
+  });
+  }
 document.body.appendChild(s);
+}
