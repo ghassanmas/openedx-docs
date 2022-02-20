341e1e752508abdfa39c510fcbda597592ce7435
Ghassan Maslamani <ghassan.maslamani@gmail.com>
2022-02-20 19:51:26 +0200

    add TX stuff

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,8 @@ exclude_patterns = ['links.rst', 'reusables/*', 'SFD_mathformatting.rst']
 tags.add('Open_edX')
 product = 'Open_edX'
 set_audience(OPENEDX, LEARNERS)
-
-
+html_static_path = ['_static']
+the_builder = 'html'
 def setup(app):
+    app.add_js_file('.tx.js')
     app.add_config_value('product', '', True)
