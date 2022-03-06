f667399ade97fd707c397dfd5b25a827ea45f24c
Ghassan Maslamani <ghassan.maslamani@gmail.com>
2022-03-06 13:25:43 +0200

    not bad

--- create_transifex_project.py
+++ create_transifex_project.py
@@ -1,10 +1,11 @@
 
 import sys
 def create_transifex_project():
-    release_name = sys.argv[1]
-    tx_token = sys.argv[2]
-    print('I am the release name',release_name)
-    print('How dare you print me!',tx_token)
+    #release_name = sys.argv[1]
+    #tx_token = sys.argv[2]
+    #print('I am the release name',release_name)
+    #print('How dare you print me!',tx_token)
+    print('here are the args',sys.argv)
 
 
 if __name__ == "__main__":
